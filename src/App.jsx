4a3bff78dde82f85ae66a2f09e8dd1460cff9d4a
Eklavya2003 <22052381@kiit.ac.in>
2024-08-03 15:44:38 +0530

    Experience section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 // import PortFolio from "./components/PortFolio";
-// import Experiance from "./components/Experiance";
+import Experiance from "./components/Experiance";
 // import Footer from "./components/Footer";
 // import Contact from "./components/Contact";
 import { Toaster } from "react-hot-toast";
@@ -15,6 +15,7 @@ function App() {
         <Navbar />
         <Home />
         <About />
+        <Experiance />
       </div>
       <Toaster />
     </>
